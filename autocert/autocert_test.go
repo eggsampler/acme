0f5ab4d78bc708c76f9f580b8fe8c1892bb95f24
isaac <git@eggsampler.com>
2018-06-12 23:58:47 +1000

    Start adding more tests for autocert

--- autocert/autocert_test.go
+++ autocert/autocert_test.go
@@ -1,6 +1,10 @@
 package autocert
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestWhitelistHosts(t *testing.T) {
 	w := WhitelistHosts("hello")
@@ -11,5 +15,16 @@ func TestWhitelistHosts(t *testing.T) {
 
 	if err := w("hello"); err != nil {
 		t.Fatalf("expected no error, got: %v", err)
+	}
+}
+
+func TestAutoCert_HTTPHandler(t *testing.T) {
+	a := AutoCert{}
+	handler := a.HTTPHandler(nil)
+	r := httptest.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	handler.ServeHTTP(w, r)
+	if w.Result().StatusCode != http.StatusMovedPermanently {
+		t.Fatalf("expected status %d, got: %d", http.StatusMovedPermanently, w.Result().StatusCode)
 	}
 }
