ba421b6183e9286e618a6c4442572fc30b1b048d
Roland Shoemaker <rolandshoemaker@gmail.com>
2019-11-11 11:13:25 -0800

    Create an HTTP client in NewClient

--- acme.go
+++ acme.go
@@ -25,11 +25,11 @@ const (
 
 // NewClient creates a new acme client given a valid directory url.
 func NewClient(directoryURL string, options ...OptionFunc) (Client, error) {
-	httpClient := http.DefaultClient
-
-	// Set a default http timeout of 60 seconds
-	// can be overridden via OptionFunc eg: acme.NewClient(url, WithHTTPTimeout(10 * time.Second))
-	httpClient.Timeout = 60 * time.Second
+	// Set a default http timeout of 60 seconds, this can be overridden
+	// via an OptionFunc eg: acme.NewClient(url, WithHTTPTimeout(10 * time.Second))
+	httpClient := &http.Client{
+		Timeout: 60 * time.Second,
+	}
 
 	acmeClient := Client{
 		httpClient: httpClient,
