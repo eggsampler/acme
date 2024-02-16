a9f5328eafb85bd8f6e255cddb012df1ab314eac
Shiloh Heurich <sheurich@fastly.com>
2024-02-16 17:12:20 -0500

    feat(challenge): add dns-account-01 support from draft rfc

--- misc_test.go
+++ misc_test.go
@@ -18,3 +18,20 @@ func TestWildcard(t *testing.T) {
 		t.Fatalf("error verifying hostname %s: %v", d, err)
 	}
 }
+
+func TestWildcardDNSAccount(t *testing.T) {
+	d := "*." + randString() + ".com"
+	account, order, _ := makeOrderFinalised(t, []string{ChallengeTypeDNSAccount01}, Identifier{Type: "dns", Value: d})
+
+	certs, err := testClient.FetchCertificates(account, order.Certificate)
+	if err != nil {
+		t.Fatalf("error fetch cert: %v", err)
+	}
+	if len(certs) == 0 {
+		t.Fatal("no certs")
+	}
+
+	if err := certs[0].VerifyHostname(d); err != nil {
+		t.Fatalf("error verifying hostname %s: %v", d, err)
+	}
+}
